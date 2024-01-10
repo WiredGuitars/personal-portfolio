7ddc1fe826910b81becca94feb7d3c09f7c650e0
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2024-01-10 15:44:40 -0600

    initial setup, two blurry circles in hero section

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Daniel Laschanzky | Portfolio",
+  description: "Portfolio website for Daniel Laschanzky, full-stack developer",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className}
+      bg-gray-50 text-gray-950 relative`}>
+        <div className="bg-[#e0f2fe] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
+        <div className="bg-[#fcfee0] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] x1:left-[-15rem] 2x1:left-[-5rem] "></div>
+        {children}
+      </body>
+    </html>
+  );
+}
