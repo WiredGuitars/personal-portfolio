b3d53cb2b28730458fe3a0822ca9ac032fb253be
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2024-01-21 16:15:04 -0600

    went back to old technologies format for now, I know it doesn't follow D.R.Y principle, but I'm still thinking about how to I want to reduce the code

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,22 +1,30 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import SectionHeading from "./SectionHeading";
 import { motion } from "framer-motion";
-
+import { DiMongodb } from "react-icons/di";
+import {
+  FaCss3Alt,
+  FaHtml5,
+  FaLinux,
+  FaNode,
+  FaReact,
+  FaSass,
+} from "react-icons/fa";
+import {
+  SiExpress,
+  SiPug,
+  SiTailwindcss,
+  SiTypescript,
+  SiVite,
+} from "react-icons/si";
+import { TbBrandNextjs, TbBrandOauth } from "react-icons/tb";
+import { IoLogoJavascript } from "react-icons/io";
+import { IoLogoFirebase } from "react-icons/io5";
+import { FiFramer } from "react-icons/fi";
 import { useSectionInView } from "@/lib/hooks";
-import { TechData } from "@/lib/data";
-import { IconType } from "react-icons";
-
-interface Technology {
-  icons: IconType;
-  fill: string;
-  stroke: string | "none";
-  text: string;
-}
-
 export default function Technologies() {
   const { ref } = useSectionInView("Technologies", 0.8);
-
   return (
     <section ref={ref}>
       <SectionHeading>
@@ -30,34 +38,110 @@ export default function Technologies() {
         </motion.div>
       </SectionHeading>
       <motion.section
-        initial={{ opacity: 0, y: 10 }}
+        className="flex "
+        initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <div className="grid gap-4 grid-cols-1 sm:grid-cols-3">
-          {TechData.map((title) => (
-            <div
-              key={title.title}
-              className="bg-black p-4 grid text-center rounded-lg"
-            >
-              <div className="text-white flex mb-4 row-span-1 text-2xl items-center justify-center border-b-8 border-yellow-400">
-                {title.title}
+        <motion.div className="flex gap-4">
+          <div className="bg-black p-4 grid grid-rows-5 text-center rounded-lg ">
+            <div className="text-white flex mb-4 row-span-1 text-2xl items-center justify-center border-b-8 border-yellow-100">
+              Frontend
+            </div>
+            <div className="row-span-4 grid grid-cols-1 gap-2 sm:grid-cols-2 pt-2  text-center ">
+              <div className="flex flex-col items-center ">
+                <FaReact className="text-blue-500 text-2xl" />
+                <div className="text-white">React.js</div>
               </div>
-              <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 pt-2 text-center">
-                {title.technologies.map((tech?: Technology) => (
-                  <div key={tech?.text} className="flex flex-col items-center">
-                    {tech &&
-                      React.createElement(tech.icons, {
-                        className: `text-2xl`,
-                        fill: tech.fill,
-                        stroke: tech.stroke,
-                      })}
-                    <span className="text-white mb-4">{tech?.text}</span>
-                  </div>
-                ))}
+              <div className="flex flex-col items-center ">
+                <SiVite className="text-purple-500 text-2xl" />
+                <span className="text-white">Vite</span>
+              </div>
+              <div className="flex flex-col items-center ">
+                <SiTypescript className="text-blue-500 text-2xl" />
+                <span className="text-white mb-4">TypeScript</span>
+              </div>
+
+              <div className="flex flex-col items-center ">
+                <IoLogoJavascript className="text-yellow-500 text-2xl" />
+                <span className="text-white mb-4">JavaScript</span>
+              </div>
+
+              <div className="flex flex-col items-center">
+                <FaHtml5 className="text-orange-500 text-2xl" />
+                <span className="text-white mb-4">HTML</span>
+              </div>
+
+              <div className="flex flex-col items-center ">
+                <FaCss3Alt className="text-blue-500 text-2xl" />
+                <span className="text-white mb-4">CSS</span>
+              </div>
+
+              <div className="flex flex-col items-center ">
+                <FaSass className="text-pink-500 text-2xl" />
+                <span className="text-white mb-4">Sass</span>
+              </div>
+
+              <div className="flex flex-col items-center">
+                <SiTailwindcss className="text-blue-500 text-2xl" />
+                <span className="text-white mb-4">Tailwind.css</span>
+              </div>
+
+              <div className="flex flex-col items-center ">
+                <TbBrandNextjs className="text-green-500 text-2xl" />
+                <span className="text-white mb-4">Next.js</span>
+              </div>
+              <div className="flex flex-col items-center ">
+                <FiFramer className="text-green-500 text-2xl" />
+                <span className="text-white mb-4">Framer</span>
+              </div>
+              <div className="flex flex-col items-center ">
+                <SiPug className="text-red-500 text-2xl" />
+                <span className="text-white">Pug.js</span>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+
+          <div className="bg-black p-4 grid grid-rows-4 text-center rounded-lg">
+            <div className="text-white flex mb-6 row-span-1 text-2xl items-center justify-center border-b-8 border-yellow-100">
+              Backend
+            </div>
+
+            <div className="row-span-3 gap-4 grid grid-cols-1">
+              <div className="flex-col flex items-center">
+                <SiExpress className="text-yellow-500 text-2xl" />
+                <span className="text-white">Express</span>
+              </div>
+              <div className="flex-col flex items-center">
+                <DiMongodb className="text-green-500 text-2xl" />
+                <span className="text-white">Mongo.db</span>
+              </div>
+              <div className="flex-col flex items-center">
+                <FaNode className="text-green-500 text-2xl" />
+                <span className="text-white">Node.js/npm</span>
+              </div>
+              <div className="flex-col flex items-center">
+                <TbBrandOauth className="text-yellow-500 text-2xl" />
+                <span className="text-white">Oauth</span>
+              </div>
+            </div>
+
+            <div className="bg-black p-4 grid grid-rows-4 text-center rounded-lg mt-4">
+              <div className="text-white flex mb-6 row-span-1 text-2xl items-center justify-center border-b-8 border-yellow-100">
+                Misc
+              </div>
+              <div className="row-span-3 grid grid-rows-2">
+                <div className="row-span-1 flex flex-col items-center justify-center">
+                  <FaLinux className="text-blue-500 text-2xl m-1" />
+                  <div className="text-white">Linux</div>
+                </div>
+                <div className="row-span-1 flex flex-col items-center justify-center">
+                  <IoLogoFirebase className="text-yellow-500 text-2xl m-1" />
+                  <div className="text-white">Firebase</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
       </motion.section>
     </section>
   );
