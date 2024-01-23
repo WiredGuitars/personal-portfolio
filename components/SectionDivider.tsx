bb22082320f0326e6967b6fee79c11d05c3e10c1
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2024-01-23 13:33:54 -0600

    finishing dark-mode touches, project is pretty much done and ready to be hosted

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -8,7 +8,7 @@ export default function SectionDivider() {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      className="bg-gray-200 dark:bg-opacity-20 my-8 h-16 w-1 rounded-full hidden sm:block"
+      className="bg-gray-400 dark:bg-opacity-20 my-8 h-16 w-1 rounded-full hidden sm:block"
     ></motion.div>
   );
 }
