a650a9d430b69af6d94066bcbcf7cfad0faf4862
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2024-01-26 02:47:43 -0600

    updated resume, updated hero section accordingly

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -88,7 +88,7 @@ export default function Hero() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-          href="/Daniel_Laschanzky.pdf"
+          href="/Resume.pdf"
           download={true}
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full  outline-none focus:scale-110 hover:scale-110  active:scale-105 transition shadow-md borderBlack dark:bg-white/10"
         >
